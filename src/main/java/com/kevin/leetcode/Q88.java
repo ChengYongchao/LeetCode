1881b20b745001d5def7387e9a3f5a5f6a8e36b2
chengyongchao <936687045@qq.com>
2021-04-05 23:46:04 +0800

    打卡

---
+++ src/main/java/com/kevin/leetcode/Q88.java
@@ -0,0 +1,44 @@
+package com.kevin.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * <p>
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[i] <= 109
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q88 {
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int j = 0;
+            for (int i = m; i < nums1.length; i++, j++) {
+                nums1[i] = nums2[j];
+            }
+            Arrays.sort(nums1);
+        }
+    }
+}
